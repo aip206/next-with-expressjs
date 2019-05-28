7bfa5d7a89ce8869f6c728c53377e6a1fe608f04
arif <arif.priyop@gmail.com>
2019-05-28 22:06:12 +0700

    commit db

--- servers/config.js
+++ servers/config.js
@@ -8,8 +8,8 @@ var config = {
     database: {
       host     : 'localhost',
       user     : 'root',
-      password : '1234abcd',
-      database : 'temp',
+      password : 'mysecretpassword',
+      database : 'omsdb',
       insecureAuth: true
     },
     mail : {
@@ -27,8 +27,8 @@ var config = {
     database: {
       host     : 'localhost',
       user     : 'root',
-      password : '1234abcd',
-      database : 'temp',
+      password : 'mysecretpassword',
+      database : 'omsdb',
       insecureAuth: true
     },
     mail : {
